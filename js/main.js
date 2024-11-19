6e8f73d6a3409e303aa1987d9b4cfb860faa49d8
WaeBae <jrwuoft@gmail.com>
2024-11-18 21:14:32 -0500

    Refactor removed redundant code no functional changes

--- js/main.js
+++ js/main.js
@@ -6,6 +6,5 @@ async function makeReq(){
   const res = await fetch(`/api?student=${userName}`)
   const data = await res.json()
 
-  console.log(data);
   document.querySelector(".returnString").textContent = data.stringValue;
 }
